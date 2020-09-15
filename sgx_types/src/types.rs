50c9badebe6be4926721757013b1e5191acb819d
zongmin.gu <zongmin.gzm@antgroup.com>
2020-09-15 13:26:05 +0800

    Update Rust SDK for Occlum

--- sgx_types/src/types.rs
+++ sgx_types/src/types.rs
@@ -990,8 +990,8 @@ impl_enum! {
         SGX_EXCEPTION_VECTOR_BP = 3,  /* INT 3 instruction */
         SGX_EXCEPTION_VECTOR_BR = 5,  /* BOUND instruction */
         SGX_EXCEPTION_VECTOR_UD = 6,  /* UD2 instruction or reserved opcode */
-        SGX_EXCEPTION_VECTOR_GP = 13, /* General protection exception */
-        SGX_EXCEPTION_VECTOR_PF = 14, /* Page fault exception */
+        SGX_EXCEPTION_VECTOR_GP = 13, /* General protection */
+        SGX_EXCEPTION_VECTOR_PF = 14, /* Page fault */
         SGX_EXCEPTION_VECTOR_MF = 16, /* x87 FPU floating-point or WAIT/FWAIT instruction */
         SGX_EXCEPTION_VECTOR_AC = 17, /* Any data reference in memory */
         SGX_EXCEPTION_VECTOR_XM = 19, /* SSE/SSE2/SSE3 floating-point instruction */
@@ -1005,6 +1005,7 @@ impl_enum! {
     pub enum sgx_exception_type_t {
         SGX_EXCEPTION_HARDWARE = 3,
         SGX_EXCEPTION_SOFTWARE = 6,
+        SGX_EXCEPTION_SIMULATED = 7,
     }
 }
 
